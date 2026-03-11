f91dd1a88ae6a9012869ccaf67983f0b6b18b2cb
A S M Sayem <scode43@gmail.com>
2026-03-11 23:51:10 +0900

    Add Camera and Geolocation plugins
    Register and enable Capacitor Camera and Geolocation native plugins for Android and iOS. For Android, include ':capacitor-camera' and ':capacitor-geolocation' projects in capacitor.settings.gradle and add implementation lines in android/app/capacitor.build.gradle. For iOS, add CapacitorCamera and CapacitorGeolocation SwiftPM packages and include their products in the app target dependencies in ios/App/CapApp-SPM/Package.swift. This enables native support for camera and geolocation functionality.

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -12,6 +12,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.1"),
+        .package(name: "CapacitorCamera", path: "../../../node_modules/@capacitor/camera"),
+        .package(name: "CapacitorGeolocation", path: "../../../node_modules/@capacitor/geolocation"),
         .package(name: "CapacitorKeyboard", path: "../../../node_modules/@capacitor/keyboard")
     ],
     targets: [
@@ -20,6 +22,8 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CapacitorCamera", package: "CapacitorCamera"),
+                .product(name: "CapacitorGeolocation", package: "CapacitorGeolocation"),
                 .product(name: "CapacitorKeyboard", package: "CapacitorKeyboard")
             ]
         )
